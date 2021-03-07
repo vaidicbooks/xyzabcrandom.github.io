ed05ea0c36e4427a567c76b514beddda50d0b65d
vaidicbooks <vaidicbooks@users.noreply.github.com>
2021-03-07 17:35:21 +0530

    added isha/2

--- upnishad/isha/java.js
+++ upnishad/isha/java.js
@@ -1,8 +1,8 @@
-        function fire() {
-            var name = document.getElementById("select").value;
-            if (name="") {
-                alert("चुनें")
-            } else {
-                window.location = "https://vedicbooks.cf/upnishad/isha/" +name;
-            }
+function fire() {
+  var page = document.getElementById('select').value;
+    if (page == "") {
+        window.location = "https://vedicbooks.cf/upnishad/isha"      
+     } else {
+         window.location= "https://vedicbooks.cf/upnishad/isha/" +page;     
         }
+}
