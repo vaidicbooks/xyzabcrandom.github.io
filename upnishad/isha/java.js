1a3aad7f0ded5f0a1921e5d8a4cca6d4643380da
vaidicbooks <vaidicbooks@users.noreply.github.com>
2021-03-15 10:38:36 +0530

    Update java.js

--- upnishad/isha/java.js
+++ upnishad/isha/java.js
@@ -1,8 +1,8 @@
 function fire() {
   var page = document.getElementById('select').value;
     if (page == "") {
-        window.location = "https://vedicbooks.cf/upnishad/isha"      
+        window.location = "http://vedicbooks.cf/upnishad/isha"      
      } else {
-         window.location= "https://"+url+"/upnishad/isha/" +page;     
+         window.location= "http://"+url+"/upnishad/isha/" +page;     
         }
 }
